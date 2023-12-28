3ad0e483646cc1141ce2bed14a236c65e10ca0da
Ashwin Essan <94974005+aeshwin10@users.noreply.github.com>
2023-12-28 09:56:00 +0530

    Time: 8 ms (73.76%), Space: 42.2 MB (17.49%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int[] sArr = new int[127];   //This is an empty array filled with zeros.
+        int[] tArr = new int[127];   //Explicitly 128 becuase there are 127 ascii values.
+                                     //200 is also possible.
+        for(int i = 0; i<s.length(); i++){
+            if (sArr[s.charAt(i)]!=tArr[t.charAt(i)]){
+                return false;
+            }
+            sArr[s.charAt(i)] = i+1;
+            tArr[t.charAt(i)] = i+1;
+        }
+        return true;   
+    }
+}
