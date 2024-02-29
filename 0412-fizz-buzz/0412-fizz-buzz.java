000703d32f3624c267a9bcdbbf3bf337aa389a19
Ashwin Essan <94974005+aeshwin10@users.noreply.github.com>
2024-02-29 23:42:53 +0530

    Time: 1 ms (99.50%), Space: 44.2 MB (99.80%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<String>();
+        for(int i = 1; i<=n; i++){
+            if (i%15 == 0){
+                res.add("FizzBuzz");
+                String.valueOf(i);
+            }
+            else if (i%5 == 0){
+                res.add("Buzz");
+            }
+            else if (i%3 == 0){
+                res.add("Fizz");
+            }
+            else{
+                String str = Integer.toString(i);
+                res.add(str);
+            }
+        }
+        return res;
+    }
+}
