1b35a16c47271098eaa78d90539f8d6bc8994606
Ashwin Essan <94974005+aeshwin10@users.noreply.github.com>
2024-06-06 16:58:13 +0530

    Time: 0 ms (100%), Space: 9.8 MB (48.18%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -9,6 +9,9 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+ //Recursive Solution:
+ /*
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
@@ -23,5 +26,24 @@ public:
         ans.push_back(root -> val);
         preorder(root-> left, ans);
         preorder(root -> right, ans);
+    }
+};
+*/
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> ans;
+        if(root==NULL) return ans;
+        
+        st.push(root);
+        while(!st.empty()){
+            root=st.top();
+            st.pop();
+            ans.push_back(root -> val);
+            if(root->right != NULL) st.push(root -> right);
+            if(root->left != NULL) st.push(root -> left);
+        }
+        return ans;
     }
 };
