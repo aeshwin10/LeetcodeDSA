c18210b20f200ce9f6cdbae0a39486dfb9936334
Ashwin Essan <94974005+aeshwin10@users.noreply.github.com>
2024-06-06 18:48:26 +0530

    Time: 4 ms (31.61%), Space: 9.8 MB (72.37%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -6,9 +6,13 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
+
+// Recursive Method:
+/*
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
@@ -23,5 +27,29 @@ public:
         inorder(root -> left, ans);
         ans.push_back(root -> val);
         inorder(root -> right, ans);
+    }
+};
+*/
+
+// Iterative Method:
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> st;
+
+        while(!st.empty() || root!=NULL){
+            while(root){
+                st.push(root);
+                root = root -> left;
+            }
+            root=st.top();
+            st.pop();
+            ans.push_back(root->val);       
+
+            root = root -> right;
+        }
+
+        return ans;
     }
 };
