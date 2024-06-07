4310d8f21e40c7ddd783c3cadbcc2a7f8206251c
Ashwin Essan <94974005+aeshwin10@users.noreply.github.com>
2024-06-07 07:52:12 +0530

    Time: 5 ms (9.51%), Space: 9.9 MB (63.41%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -28,8 +28,11 @@ public:
     }
 };
 */
-//Iterative Solution:  TC -> O(2N) for each element first inserting, then coming back.
+//Iterative Solution(single stack):  
+//                     TC -> O(2N) for each element first inserting, then coming back.
 //                     SC -> O(N) inserting atmost N elements into the stack.
+
+/*
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
@@ -60,14 +63,42 @@ public:
                     cur = st.top()->right;
                 }
             }
-            
-                
-                    
-            
         }
-
-
         return ans;
     }
-    
+};
+*/
+
+//Iterative Solution(double stack):
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> st1;
+        stack<TreeNode*> st2;
+        if(root==NULL){
+            return ans;
+        }
+        
+        st1.push(root);
+        
+        while(!st1.empty()){
+            TreeNode* temp=st1.top();
+            st1.pop();
+            st2.push(temp);
+
+
+            if(temp->left != NULL){
+                st1.push(temp->left);
+            }
+            if(temp->right != NULL){
+                st1.push(temp->right);
+            }
+        }
+        while(!st2.empty()){
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
+        return ans;
+    }
 };
