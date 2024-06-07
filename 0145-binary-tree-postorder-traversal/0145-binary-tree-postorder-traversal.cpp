b5d4d767bab495415b601e2f46586db9410ed589
Ashwin Essan <94974005+aeshwin10@users.noreply.github.com>
2024-06-07 07:26:10 +0530

    Time: 0 ms (100%), Space: 9.9 MB (63.41%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //Recursive Solution:
+ /*
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
@@ -24,4 +26,47 @@ public:
         postorder(root -> right, ans);
         ans.push_back(root -> val);
     }
+};
+*/
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
+        
+        while(!st.empty() || cur!= NULL){
+            if(cur!=NULL){
+                st.push(cur);
+                cur=cur->left;
+            }
+            else{
+                TreeNode* temp=st.top()->right;
+                if(temp==NULL){
+                    temp=st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                    
+                    while(!st.empty() && temp==st.top()->right){
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+        
+                else{
+                    cur = st.top()->right;
+                }
+            }
+            
+                
+                    
+            
+        }
+
+
+        return ans;
+    }
+    
 };
