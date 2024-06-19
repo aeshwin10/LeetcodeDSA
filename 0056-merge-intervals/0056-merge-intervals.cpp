83b85a3060b3640c7f054df3176fa49a66b44699
Ashwin Essan <94974005+aeshwin10@users.noreply.github.com>
2024-06-19 10:29:28 +0530

    Time: 17 ms (91.61%), Space: 22.7 MB (67.98%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,14 +3,16 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
         sort(intervals.begin(), intervals.end());
-        for(int i = 0; i<intervals.size(); i++){
-            if(ans.empty() || (intervals[i][0]>ans.back()[1])){
-                ans.push_back(intervals[i]);
-            }
-            else if(intervals[i][0]<=ans.back()[1]){
+        ans.push_back(intervals[0]);
+
+        for(int i = 1; i<intervals.size(); i++){
+            if(intervals[i][0]<=ans.back()[1]){
                 ans.back()[1] = max(intervals[i][1],ans.back()[1]);
             }
+            else{
+                ans.push_back(intervals[i]);
+            }
         }
-        return ans;
-    }
+        return ans;  
+    }  
 };
