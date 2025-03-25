66e71dc8d78c79e43843c2fc4a9a787d1b6c34d1
Ashwin Essan <94974005+aeshwin10@users.noreply.github.com>
2025-03-25 20:50:18 +0530

    Time: 0 ms (100%), Space: 8.6 MB (17.16%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int Vishal(int n, vector<int>& dp){
-        if(n==1 || n==0){
-            return dp[n];
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        dp[n] = Vishal(n-1, dp) + Vishal(n-2, dp);
-        return dp[n];
-    }
     int climbStairs(int n) {  
-        vector<int> dp(n+1, -1);
-        if(n==1){
-            return 1;
+        if(n==1 || n==2){
+            return n;
         }
+        vector<int> dp(n+1, 0);
         dp[1] = 1;
         dp[2] = 2;
-        return Vishal(n, dp);
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2]; 
+        }
+        return dp[n];
     }
 };
