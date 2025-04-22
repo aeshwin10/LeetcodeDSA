31a796a434736f1b00f6fcf6e86ad50370123982
Ashwin Essan <94974005+aeshwin10@users.noreply.github.com>
2025-04-22 22:51:29 +0530

    Time: 245 ms (9.18%), Space: 10.4 MB (55.05%) - LeetHub

---
+++ 2415-count-the-number-of-ideal-arrays/2415-count-the-number-of-ideal-arrays.cpp
@@ -0,0 +1,50 @@
+class Solution {
+const int MOD = 1e9 + 7;
+#define ll long long
+ll count[15][10005];
+ll prefix_sum[15][10005];
+ll options[15];
+
+void countUniqueSeq(int curr, int idx, int& maxValue){
+    options[idx] += 1;
+    for(int multiplier=2; curr*multiplier <= maxValue; multiplier++){
+        countUniqueSeq(curr*multiplier, idx+1, maxValue);
+    }
+}
+
+public:
+    int idealArrays(int n, int maxValue) {
+        memset(count, 0, sizeof(count));
+        memset(prefix_sum, 0, sizeof(prefix_sum));
+        memset(options, 0, sizeof(options));
+
+        for(int i=1; i<=10000; i++){
+            count[1][i] = 1;
+            prefix_sum[1][i] = i;
+        }
+        
+        for(int i=2; i<=14; i++){
+            for(int j=i; j<=10000; j++){
+                count[i][j] = prefix_sum[i-1][j-1];
+                prefix_sum[i][j] = count[i][j] + prefix_sum[i][j-1];
+                count[i][j] %= MOD;
+                prefix_sum[i][j] %= MOD;
+            }
+        }
+
+        for(int i=1; i<=maxValue; i++){
+            countUniqueSeq(i, 1, maxValue);
+        }
+        
+        ll ans;
+        ll ways;
+        for(int i=2; i<=14; i++){
+            ways = count[i][n] * options[i];
+            ways %= MOD;
+            ans += ways;
+            ans %= MOD;
+        }
+        return ans;
+
+    }
+};
