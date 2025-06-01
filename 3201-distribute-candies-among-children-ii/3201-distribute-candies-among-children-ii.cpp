c4665675a3aad44c09c670ea8f7d4ed5b9ff8a14
Ashwin Essan <94974005+aeshwin10@users.noreply.github.com>
2025-06-01 23:04:21 +0530

    Time: 26 ms (50.59%), Space: 8.9 MB (57.06%) - LeetHub

---
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long distributeCandies(int n, int limit) {
+        long long ans = 0;
+        for(int a = max(0, n - (2 * limit)); a<=min(limit, n); a++){
+            int b = min(limit, n-a) - max(0, n-a-limit) + 1;
+            ans+= max(0,b);
+        }
+        return ans;
+    }
+};
